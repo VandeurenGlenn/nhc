3af098cf741a54b81d97ff1cee6f93160b9a7a2f
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-12-01 11:19:02 +0100

    0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     description='SDK for Niko Home Control',
     license='MIT',
     url='https://github.com/vandeurenglenn/nhc',
-    version='0.2.0',
+    version='0.2.3',
     author='vandeuren Glenn',
     author_email='vandeurenglenn@gmail.com',
     maintainer='vandeuren Glenn',
