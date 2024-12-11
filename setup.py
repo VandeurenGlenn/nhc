8b57ddf4ce74f3b9e4eeb9a9c9e705e7e4357780
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2024-12-11 19:49:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     description='SDK for Niko Home Control',
     license='MIT',
     url='https://github.com/vandeurenglenn/nhc',
-    version='0.2.14',
+    version='0.2.15',
     author='vandeuren Glenn',
     author_email='vandeurenglenn@gmail.com',
     maintainer='vandeuren Glenn',
