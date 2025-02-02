d4ec242215695eb8b9f6567025e52a87c5f4988c
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-02-02 14:24:00 +0100

    setup: remove nclib requirement & add asyncio

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description='Niko Home Control Client Library',
     packages=setuptools.find_packages(),
     zip_safe=False,
-    install_requires=['nclib'],
+    install_requires=['asyncio'],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
