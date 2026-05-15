b01aff0ed85d444ad24fdd6c7e62c9f5c59923ec
jeroenvaes <github@jeroenvaes.fastmail.com>
2026-05-15 18:17:10 +0200

    Fix the dimmer to remember the previous value when brightness parameter is omitted

--- nhc/light.py
+++ nhc/light.py
@@ -11,14 +11,20 @@ class NHCLight(NHCAction):
     async def turn_on(self, brightness: Optional[int] = None) -> None:
         """Turn On."""
         if (brightness is None):
-            brightness = self._state if self._state > 0 else 100
+            if self.is_dimmable:
+                brightness = 254
+            else:
+                brightness = self._state if self._state > 0 else 100
         else:
             brightness = round(brightness / 2.55)
         await self._controller.execute(self.id, brightness)
 
     async def turn_off(self) -> None:
         """Turn off."""
-        await self._controller.execute(self.id, 0)
+        value = 0
+        if self.is_dimmable:
+            value = 255
+        await self._controller.execute(self.id, value)
 
     async def toggle(self) -> None:
         """Toggle on/off."""
