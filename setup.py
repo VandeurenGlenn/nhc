21bad83c22f8a08659a151a94abd54fcb5e88fa7
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2026-06-17 00:35:42 +0200

    Bump version from 0.8.0 to 0.8.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     description='SDK for Niko Home Control',
     license='MIT',
     url='https://github.com/vandeurenglenn/nhc',
-    version='0.8.0',
+    version='0.8.1',
     author='Vandeuren Glenn',
     author_email='vandeurenglenn@gmail.com',
     maintainer='Vandeuren Glenn',
